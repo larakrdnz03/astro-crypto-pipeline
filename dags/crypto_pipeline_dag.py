53efbbfbc64dde762b2f593745fd732e3b96862f
lara <laraminakaradeniz@192.168.1.106>
2025-07-28 10:35:12 +0300

    Replace astronaut DAG with crypto pipeline DAG

--- dags/crypto_pipeline_dag.py
+++ dags/crypto_pipeline_dag.py
@@ -1,100 +1,61 @@
-"""
-## Astronaut ETL example DAG
-
-This DAG queries the list of astronauts currently in space from the
-Open Notify API and prints each astronaut's name and flying craft.
-
-There are two tasks, one to get the data from the API and save the results,
-and another to print the results. Both tasks are written in Python using
-Airflow's TaskFlow API, which allows you to easily turn Python functions into
-Airflow tasks, and automatically infer dependencies and pass data.
-
-The second task uses dynamic task mapping to create a copy of the task for
-each Astronaut in the list retrieved from the API. This list will change
-depending on how many Astronauts are in space, and the DAG will adjust
-accordingly each time it runs.
-
-For more explanation and getting started instructions, see our Write your
-first DAG tutorial: https://www.astronomer.io/docs/learn/get-started-with-airflow
-
-![Picture of the ISS](https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2010/02/space_station_over_earth/10293696-3-eng-GB/Space_Station_over_Earth_card_full.jpg)
-"""
-
-from airflow.sdk.definitions.asset import Asset
-from airflow.decorators import dag, task
-from pendulum import datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import pandas as pd
 import requests
+from sqlalchemy import create_engine
+import ta
 
+NEON_DB_URL = "postgresql://<username>:<password>@<host>:5432/<dbname>?sslmode=require"
 
-# Define the basic parameters of the DAG, like schedule and start_date
-@dag(
-    start_date=datetime(2024, 1, 1),
-    schedule="@daily",
-    catchup=False,
-    doc_md=__doc__,
-    default_args={"owner": "Astro", "retries": 3},
-    tags=["example"],
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2024, 1, 1),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG(
+    'crypto_pipeline_dag',
+    default_args=default_args,
+    schedule_interval='@hourly',
+    catchup=False
 )
-def example_astronauts():
-    # Define tasks
-    @task(
-        # Define a dataset outlet for the task. This can be used to schedule downstream DAGs when this task has run.
-        outlets=[Asset("current_astronauts")]
-    )  # Define that this task updates the `current_astronauts` Dataset
-    def get_astronauts(**context) -> list[dict]:
-        """
-        This task uses the requests library to retrieve a list of Astronauts
-        currently in space. The results are pushed to XCom with a specific key
-        so they can be used in a downstream pipeline. The task returns a list
-        of Astronauts to be used in the next task.
-        """
-        try:
-            r = requests.get("http://api.open-notify.org/astros.json")
-            r.raise_for_status()
-            number_of_people_in_space = r.json()["number"]
-            list_of_people_in_space = r.json()["people"]
-        except Exception:
-            print("API currently not available, using hardcoded data instead.")
-            number_of_people_in_space = 12
-            list_of_people_in_space = [
-                {"craft": "ISS", "name": "Oleg Kononenko"},
-                {"craft": "ISS", "name": "Nikolai Chub"},
-                {"craft": "ISS", "name": "Tracy Caldwell Dyson"},
-                {"craft": "ISS", "name": "Matthew Dominick"},
-                {"craft": "ISS", "name": "Michael Barratt"},
-                {"craft": "ISS", "name": "Jeanette Epps"},
-                {"craft": "ISS", "name": "Alexander Grebenkin"},
-                {"craft": "ISS", "name": "Butch Wilmore"},
-                {"craft": "ISS", "name": "Sunita Williams"},
-                {"craft": "Tiangong", "name": "Li Guangsu"},
-                {"craft": "Tiangong", "name": "Li Cong"},
-                {"craft": "Tiangong", "name": "Ye Guangfu"},
-            ]
 
-        context["ti"].xcom_push(
-            key="number_of_people_in_space", value=number_of_people_in_space
-        )
-        return list_of_people_in_space
+def fetch_process_store():
+    url = "https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=100"
+    response = requests.get(url)
+    data = response.json()
 
-    @task
-    def print_astronaut_craft(greeting: str, person_in_space: dict) -> None:
-        """
-        This task creates a print statement with the name of an
-        Astronaut in space and the craft they are flying on from
-        the API request results of the previous task, along with a
-        greeting which is hard-coded in this example.
-        """
-        craft = person_in_space["craft"]
-        name = person_in_space["name"]
+    df = pd.DataFrame(data, columns=[
+        'open_time', 'open', 'high', 'low', 'close', 'volume',
+        '_1', '_2', '_3', '_4', '_5', '_6'
+    ])
+    df = df[['open_time', 'open', 'high', 'low', 'close', 'volume']]
+    df = df.astype(float)
+    df['open_time'] = pd.to_datetime(df['open_time'], unit='ms')
 
-        print(f"{name} is currently in space flying on the {craft}! {greeting}")
+    df['sma'] = ta.trend.sma_indicator(df['close'], window=14)
+    df['ema'] = ta.trend.ema_indicator(df['close'], window=14)
+    df['rsi'] = ta.momentum.RSIIndicator(df['close'], window=14).rsi()
+    
+    engine = create_engine(NEON_DB_URL)
+    df.to_sql('btc_usdt_technical', engine, if_exists='append', index=False)
 
-    # Use dynamic task mapping to run the print_astronaut_craft task for each
-    # Astronaut in space
-    print_astronaut_craft.partial(greeting="Hello! :)").expand(
-        person_in_space=get_astronauts()  # Define dependencies using TaskFlow API syntax
+with dag:
+    fetch_store = PythonOperator(
+        task_id='fetch_process_store',
+        python_callable=fetch_process_store
     )
 
 
-# Instantiate the DAG
-example_astronauts()
+
+
+
+
+
+
+
+
+
+
